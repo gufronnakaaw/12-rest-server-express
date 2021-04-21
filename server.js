ff1515e4865dd03c03d3f696500b8a451fe753b6
gufronnakaaw <gufronnakaaw@gmail.com>
2021-04-21 12:59:25 +0700

    rename

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const StudentRoute = require('./component/StudentsRoute')
+const StudentsRoute = require('./component/StudentsRoute')
 const cors = require('cors')
 
 app.use(cors())
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.use('/api/v1', StudentRoute)
+app.use('/api/v1', StudentsRoute)
 
 app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`)
